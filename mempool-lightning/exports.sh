f3109eec22201488acf1134623e8d981f5bd9918
Felipe Knorr Kuhn <100320+knorrium@users.noreply.github.com>
2023-01-11 22:27:06 -0800

    Initial test version of the Lightning enabled app

---
+++ mempool-lightning/exports.sh
@@ -0,0 +1,14 @@
+export APP_MEMPOOL_IP="10.21.21.26"
+export APP_MEMPOOL_PORT="3006"
+export APP_MEMPOOL_API_IP="10.21.21.27"
+export APP_MEMPOOL_LIGHTNING_API_IP="10.21.21.29"
+
+export APP_MEMPOOL_DB_IP="10.21.21.28"
+
+# export APP_MEMPOOL_LIGHTNING_IP="10.21.21.26"
+# export APP_MEMPOOL_LIGHTNING_PORT="3006"
+# export APP_MEMPOOL_LIGHTNING_API_IP="10.21.21.27"
+# export APP_MEMPOOL_LIGHTNING_DB_IP="10.21.21.28"
+
+hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-$1{EXPORTS_APP_ID}/hostname"
+export APP_MEMPOOL_HIDDEN_SERVICE="$(cat "${hidden_service_file}" 2>/dev/null || echo "")"
